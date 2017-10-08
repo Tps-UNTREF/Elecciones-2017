b309ceb5cdea79f1ff5b246e32ba42578aff49e4
Mati <mati.juguera@gmail.com>
2017-10-07 21:25:26 -0300

    la weaaaaaaaaa

--- Main.py
+++ Main.py
@@ -1,8 +1,8 @@
 import time
 
 from twitter import *
-import json
 
+import Persistencia
 from config import ACCESS_TOKEN_KEY, ACCESS_TOKEN_SECRET, CONSUMER_SECRET, CONSUMER_KEY
 
 class Main():
@@ -13,7 +13,8 @@ class Main():
         # Nos identificamos con twitter
         self.tw = Twitter(auth = OAuth(ACCESS_TOKEN_KEY,ACCESS_TOKEN_SECRET,CONSUMER_KEY,CONSUMER_SECRET))
         self.candidatos=["@CFKArgentina", "@estebanbullrich" , "@SergioMassa" , "@RandazzoF" , "@nestorpitrola", "@JorgeTaiana" , "@gladys_gonzalez" , "@Stolbizer" , "@andreadatri"]
-        self.busqueda(candidatos)
+        almacenamiento = almacenamiento = {'@CFKArgentina': {}, '@estebanbullrich': {}, '@SergioMassa': {}, '@RandazzoF': {},'@nestorpitrola': {},'@JorgeTaiana': {}, '@gladys_gonzalez': {}, '@Stolbizer': {}, '@andreadatri': {}}
+        self.busqueda(self.candidatos,almacenamiento)
 
 
 
@@ -23,7 +24,6 @@ class Main():
         candidatos_OR = str(' OR ').join(lista)
 
         contador = 0
-
 
         resultados = self.tw.search.tweets(q=candidatos_OR, result_type='recent', count=100)
         ultimo_id = resultados['statuses'][0]['id']
@@ -40,7 +40,7 @@ class Main():
 
 
 
-        for x in range(0,10):
+        for x in range(0,3):
             time.sleep(15)
             resultados = self.tw.search.tweets(q=candidatos_OR,result_type='recent',count=1, since_id= ultimo_id)
             if resultados['statuses'] !=[]:
@@ -50,18 +50,15 @@ class Main():
                     id = tweet['id']
                     print(ascii(tweet['text']))
                     text = tweet['text']
-                    for candidato in candidatos:
+                    for candidato in lista:
                         if candidato in text:
                             if id not in almacenamiento[candidato]:
                                 almacenamiento[candidato][id] = {'Hora': resultados['statuses'][0]['created_at'],
                                                                  'Texto': text}
-                                contador += 1
+
+        Persistencia.guardar(almacenamiento)
 
 
-
-
-        print(contador)
-        print(almacenamiento)
 
 
 
---
+++ Persistencia.py
@@ -0,0 +1,26 @@
+import json
+import os
+
+import pickle
+
+
+def cargar():
+    almacenamiento = {'@CFKArgentina': {}, '@estebanbullrich': {}, '@SergioMassa': {}, '@RandazzoF': {},'@nestorpitrola': {},'@JorgeTaiana': {}, '@gladys_gonzalez': {}, '@Stolbizer': {}, '@andreadatri': {}}
+    for candidato in ["@CFKArgentina", "@estebanbullrich" , "@SergioMassa" , "@RandazzoF" , "@nestorpitrola", "@JorgeTaiana" , "@gladys_gonzalez" , "@Stolbizer" , "@andreadatri"] :
+        file = open(os.getcwd() + '\\' + str(candidato).replace('@', '').lower() + '.j', 'r')
+        lista = json.load(file)
+        for id,fecha_texto in lista.items():
+                almacenamiento[candidato][id] = fecha_texto
+        file.close()
+    return almacenamiento
+
+def guardar(almacenamiento):
+    for candidato, dic_ids in almacenamiento.items():
+        diccionario = dic_ids
+        file = open(os.getcwd() + '\\' + str(candidato).replace('@', '').lower() + '.j', 'w')
+        json.dump(diccionario, file)
+        file.close()
+
+def guarda_estadistica(ultimo_id,cantidad_tweets):
+    file = open('Estadisticas.p', 'wb')
+    pickle.dump()
